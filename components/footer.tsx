d7967fcdf50c18cc34caf151a72fb621acb02a01
Abdulrahman331-igtm <abdirahmanshaffy22@gmail.com>
2026-04-29 18:43:44 +0300

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -75,7 +75,7 @@ export default function Footer() {
 
       {/* COPYRIGHT BAR */}
       <div className="max-w-6xl mx-auto pt-8 border-t border-white/5 flex flex-col md:flex-row justify-between items-center gap-4 text-[10px] uppercase tracking-widest text-gray-600">
-        <p>© 2026 Parkview Mall. All rights reserved.</p>
+        <p>© 2026 Parkview Mall. All rights reserved.powered by Abdirahman.</p>
         <p className="hover:text-gray-400 transition-colors cursor-default">Designed for Excellence</p>
       </div>
     </footer>
